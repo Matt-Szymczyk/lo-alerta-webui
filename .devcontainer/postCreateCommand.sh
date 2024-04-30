8962ae7d1c0c1a19d3eaab33a80d811ca0d5dcc2
Mateusz Szymczyk <mateusz-szymczyk@outlook.com>
2024-04-30 13:06:18 +0000

    Add dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
-set -e
-set -o noglob
+# Load nvm
+source $NVM_DIR/nvm.sh
 
-
+# Install Node.js 14
 nvm install 14
+
+# Use Node.js 14
 nvm use 14
+
+# Install npm packages
 npm install
 
 pip install alerta
+
+export VUE_APP_ALERTA_ENDPOINT=http://localhost:8080/api
